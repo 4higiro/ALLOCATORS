e1229ba88c44345b3297c05d6986229e633f99ee
Egor <99319750+4higiro@users.noreply.github.com>
2023-08-26 08:58:48 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,78 +1,47 @@
 #include <iostream>
+#include <vector>
 #include <thread>
 
 #include "alloc.hpp"
 
 int main()
 {
-	const size_t n = 1E4;
+	alloc::Allocator<int> linear;
+	alloc::Allocator<int, alloc::MemoryStack> stack;
+	alloc::Allocator<int, alloc::MemoryPool> pool;
+
+	constexpr size_t n = 2E4;
 	std::cout << "Allocate + deallocate in " << n << " iterations" << std::endl;
 
-	alloc::Allocator<char, alloc::default_memory_size, alloc::MemoryLinear> linear;
 	auto begin = std::chrono::high_resolution_clock::now();
 	for (size_t i = 0; i < n; ++i)
 	{
-		char* p = linear.allocate(1);
+		int* p = linear.allocate(1);
 		linear.deallocate(p, 1);
 	}
 	auto end = std::chrono::high_resolution_clock::now();
 	std::chrono::duration<double> time = end - begin;
 	std::cout << "Linear allocator:\t" << time.count() << std::endl;
 
-	alloc::Allocator<char, alloc::default_memory_size, alloc::MemoryStack> stack;
 	begin = std::chrono::high_resolution_clock::now();
 	for (size_t i = 0; i < n; ++i)
 	{
-		char* p = stack.allocate(1);
+		int* p = stack.allocate(1);
 		stack.deallocate(p, 1);
 	}
 	end = std::chrono::high_resolution_clock::now();
 	time = end - begin;
 	std::cout << "Stack allocator:\t" << time.count() << std::endl;
 
-	alloc::Allocator<char, alloc::default_memory_size, alloc::MemoryPool> pool(4);
 	begin = std::chrono::high_resolution_clock::now();
 	for (size_t i = 0; i < n; ++i)
 	{
-		char* p = pool.allocate(1);
+		int* p = pool.allocate(1);
 		pool.deallocate(p, 1);
 	}
 	end = std::chrono::high_resolution_clock::now();
 	time = end - begin;
-	std::cout << "Pool allocator:\t\t" << time.count() << std::endl;
-
-	alloc::Allocator<char, alloc::default_memory_size, alloc::MemoryLinear, alloc::STACK> linear_static;
-	begin = std::chrono::high_resolution_clock::now();
-	for (size_t i = 0; i < n; ++i)
-	{
-		char* p = linear_static.allocate(1);
-		linear_static.deallocate(p, 1);
-	}
-	end = std::chrono::high_resolution_clock::now();
-	time = end - begin;
-	std::cout << "Static Linear allocator:" << time.count() << std::endl;
-
-	alloc::Allocator<char, alloc::default_memory_size, alloc::MemoryStack, alloc::STACK> stack_static;
-	begin = std::chrono::high_resolution_clock::now();
-	for (size_t i = 0; i < n; ++i)
-	{
-		char* p = stack_static.allocate(1);
-		stack_static.deallocate(p, 1);
-	}
-	end = std::chrono::high_resolution_clock::now();
-	time = end - begin;
-	std::cout << "Static Stack allocator:\t" << time.count() << std::endl;
-
-	alloc::Allocator<char, alloc::default_memory_size, alloc::MemoryPool, alloc::STACK> pool_static(4);
-	begin = std::chrono::high_resolution_clock::now();
-	for (size_t i = 0; i < n; ++i)
-	{
-		char* p = pool_static.allocate(1);
-		pool_static.deallocate(p, 1);
-	}
-	end = std::chrono::high_resolution_clock::now();
-	time = end - begin;
-	std::cout << "Static Pool allocator:\t" << time.count() << std::endl;
+	std::cout << "Pool allocator:  \t" << time.count() << std::endl;
 
 	begin = std::chrono::high_resolution_clock::now();
 	for (size_t i = 0; i < n; ++i)
@@ -82,7 +51,7 @@ int main()
 	}
 	end = std::chrono::high_resolution_clock::now();
 	time = end - begin;
-	std::cout << "Malloc:\t\t\t" << time.count() << std::endl;
+	std::cout << "Malloc & free:   \t" << time.count() << std::endl;
 
 	system("pause");
 	return 0;
